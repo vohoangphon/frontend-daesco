474c820b7f014aa9f90f7baa1a15726cdd1d15d8
vohoangphon <Admin@Admin.com>
2024-06-12 06:12:59 +0700

    soucre

---
+++ Footer.php
@@ -0,0 +1,75 @@
+<footer id="footer" class="footer" style="background-image: url('style/images/tmp/footer-bg.png');">
+    <div class="footer-inner">
+        <div class="container">
+            <div class="footer-middle">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="f-about">
+                            <h2>Công ty Cổ Phần Máy - Thiết Bị Dầu Khí Đà Nẵng</h2>
+                            <ul>
+                                <li>
+                                    <img src="style/uploads/2022/12/map.png" alt="icon contact">
+                                    <a href="#">51 Phan Đăng Lưu, Hòa Cường Nam, Quận Hải Châu, TP. Đà Nẵng</a>
+                                </li>
+                                <li>
+                                    <img src="style/uploads/2022/12/tel.png" alt="icon contact">
+                                    <a href="#">(84-236) 3821637 - 3823487</a>
+                                </li>
+                                <li>
+                                    <img src="style/uploads/2022/12/mail.png" alt="icon contact">
+                                    <a href="mailto:thanhpvmdaesco@gmail.com">thanhpvmdaesco@gmail.com</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="f-menu">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="item">
+                                        <ul>
+                                            <li><a href="index.php">Trang chủ</a></li>
+                                            <li><a href="quan-he-co-dong.php">Quan hệ cổ đồng</a></li>
+                                            <li><a href="lien-he.php">Liên hệ</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="item">
+                                        <ul>
+                                            <li><a href="tong-quan.php">Giới thiệu</a></li>
+                                            <li><a href="tuyen-dung.php">Tuyển dụng, Mời thầu</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="row">
+                    <div class="col-xl-7">
+                        <!-- <div class="left">
+                            <span>Để lại thông tin liên hệ</span>
+                            <ul>
+                                <li><a href="">Đơn vị cung cấp </a>
+                                </li>
+                                <li><a href="">Đơn vị thu mua</a>
+                                </li>
+                                <li><a href="">Đối tác chiến
+                                        lược</a></li>
+                            </ul>
+                        </div> -->
+                    </div>
+                    <div class="col-xl-5">
+                        <div class="right">
+                            <span>Copyright (c) 2018 Công ty Cổ Phần Máy &#8211; Thiết Bị Dầu Khí Đà Nẵng</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
